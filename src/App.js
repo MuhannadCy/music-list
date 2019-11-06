577d74085a739812ce2854294eeb6d67fe64ce5a
Muhannad Alanazi <muhannad.h.alanazi@gmail.com>
2019-11-06 16:32:30 +0300

    fixed the API request

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     this.handleFaveToggle = this.handleFaveToggle.bind(this);
   }
   componentDidMount() {
-    axios.get(`http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&limit=10&api_key=8b478cd7bd90968e2c92c09a55fe2efa&format=json`)
+    axios.get(`https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&limit=10&api_key=8b478cd7bd90968e2c92c09a55fe2efa&format=json`)
       .then(res => {
         const songs = res.data.tracks.track;
         console.log(songs)
