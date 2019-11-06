f4b5dba2ac25a57a8386f794073c1425aaa76588
Muhannad Alanazi <muhannad.h.alanazi@gmail.com>
2019-11-06 16:16:08 +0300

    Updates

---
+++ precache-manifest.4ae8b3622d44e77c8763712c67c2b0ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c517d8fb36062535184a0e74e556b0",
+    "url": "/music-list/index.html"
+  },
+  {
+    "revision": "8c399f43402a3689060f",
+    "url": "/music-list/static/css/main.1c98973d.chunk.css"
+  },
+  {
+    "revision": "ffe5147c0576dde44ed6",
+    "url": "/music-list/static/js/2.b40305e6.chunk.js"
+  },
+  {
+    "revision": "8c399f43402a3689060f",
+    "url": "/music-list/static/js/main.40e59b66.chunk.js"
+  },
+  {
+    "revision": "de4edf6bf78302ea9756",
+    "url": "/music-list/static/js/runtime-main.d8458c5e.js"
+  }
+]);
